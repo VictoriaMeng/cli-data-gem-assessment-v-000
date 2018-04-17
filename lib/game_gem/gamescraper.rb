abf75d014767c180174f4ddab493181549c9233b
Victoria Meng <nights1ststar@protonmail.com>
2018-04-17 01:54:24 +0000

    Refactored #get_names

--- lib/game_gem/gamescraper.rb
+++ lib/game_gem/gamescraper.rb
@@ -1,18 +1,21 @@
 class GameScraper
-  attr_reader :html, :fields
+  attr_reader :html, :games, :names
 
   def initialize
     @html = Nokogiri::HTML(open("http://store.steampowered.com/"))
-    @fields = []
+    @games_html = @html.css("#tab_topsellers_content")
+    @names = []
   end
 
   def get_names
-    games = @html.css("#tab_topsellers_content")
-    names = games.css(".tab_item_name")
+    names = @games_html.css(".tab_item_name")
     names.each do |name|
       game = {}
       game[:name] = name.text
-      @fields << game
+      @names << game
     end
+  end
+
+  def get_prices
   end
 end
