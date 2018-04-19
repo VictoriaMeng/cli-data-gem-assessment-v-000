3226375d71886574c1901a998ce27a8950293607
Victoria Meng <nights1ststar@protonmail.com>
2018-04-19 03:13:51 +0000

    renamed top_sellers.rb to game_gem.rb so file name matches gem name so gem can run properly. Refactored #GameScraper.run to use .map insteadd of .each

--- lib/game_gem/gamescraper.rb
+++ lib/game_gem/gamescraper.rb
@@ -1,20 +1,17 @@
 class GameScraper
-  attr_reader :games
 
   def initialize
     @html = Nokogiri::HTML(open("http://store.steampowered.com/"))
     @games_html = @html.css("#tab_topsellers_content a")
-    @games = []
   end
 
   def run
-    @games_html.each do |game|
+    @games_html.map do |game|
       fields = {}
       fields[:name] = game.css(".tab_item_name").text
       fields[:price] = game.css(".discount_final_price").text
-      @games << fields
+      fields
     end
-    @games
   end
 
 end
