7b9f5d51ff2a3ff1300a1f896e9e7cef124ef796
VictoriaMeng <1ststarofthenight@gmail.com>
2019-05-06 21:39:51 -0700

    Use 'let' to create multiple game instances To prepare for testing Game.mass_create

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,10 +1,20 @@
 require_relative '../lib/game_gem/game'
 
 RSpec.describe Game do
-  subject { Game.new({name: "Skyrim", price: "$39.99"}) }
+  let(:skyrim) { Game.new({name: "Skyrim", price: "$39.99"}) }
+  let(:fnv) { Game.new({name: "Fallout New Vegas", price: "$9.99"}) }
 
   it 'has a name and price' do
-    expect(subject.name).to eql("Skyrim")
-    expect(subject.price).to eql("$39.99")
+    expect(skyrim.name).to eql("Skyrim")
+    expect(skyrim.price).to eql("$39.99")
+    
+    expect(fnv.name).to eql("Fallout New Vegas")
+    expect(fnv.price).to eql("$9.99")
+  end
+
+  context 'when mass creating games' do 
+    before(:context) do 
+
+    end
   end
 end
