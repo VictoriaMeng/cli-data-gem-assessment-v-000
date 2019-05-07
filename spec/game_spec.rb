28d470dad2665d117e5c0343d210375ffa79d497
VictoriaMeng <1ststarofthenight@gmail.com>
2019-05-06 21:08:39 -0700

    Write example to test if Game instantiates w/ name and price

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,1 +1,10 @@
 require_relative '../lib/game_gem/game'
+
+RSpec.describe Game do
+  subject { Game.new({name: "Skyrim", price: "$39.99"}) }
+
+  it 'has a name and price' do
+    expect(subject.name).to eql("Skyrim")
+    expect(subject.price).to eql("$39.99")
+  end
+end
